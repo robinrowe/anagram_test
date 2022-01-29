824aa827bf486ea0810ded63e126c8d4d1c088bf
Robin Rowe <robin.rowe@cinepaint.org>
2022-01-28 16:59:26 -0800

    Replaced fill() with vector::ssign()

--- AnagramTest.cpp
+++ AnagramTest.cpp
@@ -26,14 +26,15 @@ bool AnagramTest::CheckAnagrams(int count,char* words[])
     }
     const size_t length = strlen(words[0]);
     anagram.resize(count);
-    fill(anagram.begin(),anagram.end(),false);
+    //fill(anagram.begin(),anagram.end(),false);
+    anagram.assign(count,false);
     anagram[0] = true;
     // Due to SSO (Short String Optimization) there is typically no heap allocation for small strings,
     // that the STL will have 10 to 20 bytes allocated in the string object typically. However, longer
     // strings will create a heap allocation and global lock, such that optimization in C may be better.
     string baseWord = words[0];
     sort(baseWord.begin(),baseWord.end());
-    for(unsigned i=0;i<count;i++)
+    for(unsigned i=1;i<count;i++)
     {   if(length != strlen(words[i]))
         {   continue;
         }
